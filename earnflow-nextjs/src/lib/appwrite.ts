b3e3abce49be00a814f9b0064ede32f0762593dc
DR1vX <harebsaeed@dr4xv.local>
2025-12-27 11:49:29 +0400

    fix: handle undefined environment variable in appwrite client

--- earnflow-nextjs/src/lib/appwrite.ts
+++ earnflow-nextjs/src/lib/appwrite.ts
@@ -2,7 +2,7 @@ import { Client, Account, Databases, ID } from 'appwrite';
 
 const client = new Client()
   .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '');
 
 export const account = new Account(client);
 export const databases = new Databases(client);
